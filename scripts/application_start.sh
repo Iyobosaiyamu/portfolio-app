9c189abd3d13032dac79c8978f678d4f34927b31
Iyobosaiyamu <48036906+Iyobosaiyamu@users.noreply.github.com>
2022-03-12 17:21:33 -0500

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,4 +4,4 @@
 forever stopall
 # forever start --minUptime 10000 --spinSleepTime 1000 /home/ec2-user/nodejs/app.js
 
-sudo forever start -c "npm start" /home/ubuntu/myportfolio/ --minUptime 10000 --spinSleepTime 1000
+sudo forever start -c "npm start" /home/ubuntu/myportfolio/ --minUptime 100000 --spinSleepTime 10000
